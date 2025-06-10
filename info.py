5fe9b77c8a94c6cfecb8e766cd21dc4905525aa0
AxonBots <148529190+AxonBots@users.noreply.github.com>
2025-06-10 11:44:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,14 +15,14 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '25776734'))
 API_HASH = environ.get('API_HASH', '9bb0c527d53d497506baf1bd17d7426c')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7873983813:AAFr-baW5FcU_SHVlIeZz7iEan-oUBd-ikg')
+BOT_TOKEN = environ.get('BOT_TOKEN', ' ')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8064881390').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/mihir_ax") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002553587888'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002638279408'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/mihir_ax')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002553587888').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002599471815').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://KRISTEENQ4:KRISTEEEEEN@cluster0.iqxzy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -30,27 +30,27 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'My_Tg_files')
 
 # Add new database configuration for files
-FILES_DATABASE_URI = environ.get('FILES_DATABASE_URI', "mongodb+srv://KRISTEENQ3:KRISTEEEEN@cluster0.luljb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # Default to same URI if not specified
+FILES_DATABASE_URI = environ.get('FILES_DATABASE_URI', "mongodb+srv://Kristeebn:ajsgdgasdga@cluster0.nii4lry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # Default to same URI if not specified
 FILES_DATABASE_NAME = environ.get('FILES_DATABASE_NAME', "Files_Database")  # New database name for files
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #----------- There will be channel id add in all these ---------
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '0'))  # set shortner log channel
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002620520711'))  # set shortner log channel
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','0')) # The movie you upload in it will be deleted from the bot.
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '0'))
-auth_channel = environ.get('AUTH_CHANNEL', '')
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002620520711'))
+auth_channel = environ.get('AUTH_CHANNEL', '-1002313843759')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
-request_channel = environ.get('REQUEST_CHANNEL', '0') # If anyone sends a request message to your bot, you will get it in this channel.
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '0')) # 
+request_channel = environ.get('REQUEST_CHANNEL', '-1002638279408') # If anyone sends a request message to your bot, you will get it in this channel.
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002638279408'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Suport_MiHiR_AX') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 IS_VERIFY = is_enabled('IS_VERIFY', False)
 #---------------------------------------------------------------
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
-TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/")
-TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/mihir_kx")
+TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/mihir_kx")
+TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/mihir_kx")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
 SHORTENER_API = environ.get("SHORTENER_API", "3097623f852197a9ce40d1212aaa8bbf2803e799")
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'omegalinks.in')
